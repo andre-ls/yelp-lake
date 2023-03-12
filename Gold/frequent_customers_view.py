b518adb4ae7a4bc587c8f6600f90d1612bba8e15
andre-ls <andre.luis.e3@gmail.com>
2023-03-12 15:18:27 -0300

    Add Frequent Customers View

---
+++ Gold/frequent_customers_view.py
@@ -0,0 +1,30 @@
+import os
+import re
+from dotenv import load_dotenv
+from pyspark.sql import SparkSession, functions as f
+
+load_dotenv()
+awsAccessKey = os.environ.get('AWS_ACCESS_KEY')
+awsAccessSecret = os.environ.get('AWS_ACCESS_SECRET')
+awsS3Directory = os.environ.get('AWS_S3_DIRECTORY')
+
+spark = SparkSession.builder.appName("Frequent Customers Gold View").getOrCreate()
+
+spark.sparkContext\
+     ._jsc.hadoopConfiguration().set("fs.s3a.access.key", awsAccessKey)
+spark.sparkContext\
+     ._jsc.hadoopConfiguration().set("fs.s3a.secret.key", awsAccessSecret)
+spark.sparkContext\
+      ._jsc.hadoopConfiguration().set("fs.s3a.endpoint", "s3.amazonaws.com")
+spark.sparkContext\
+      ._jsc.hadoopConfiguration().set("fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem")
+
+df = spark.read.option("inferSchema","true").parquet(awsS3Directory + "/Silver/tip_data")
+
+df = df.groupBy(["business_id","user_id"]).count()
+
+df.show(n=30,vertical=True,truncate=False)
+df.printSchema()
+
+#df.write.parquet(awsS3Directory + "/Gold/reviews_tips_view")
+
