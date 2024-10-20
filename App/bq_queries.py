d9116e7a303f3d0ddf1c75221235b4fbfb00dd38
andre-ls <andre.luis.e3@gmail.com>
2024-10-20 14:27:16 -0300

    Fix Streamlit Secrets Reading

--- App/bq_queries.py
+++ App/bq_queries.py
@@ -4,16 +4,16 @@ from google.oauth2 import service_account
 
 credentials = service_account.Credentials.from_service_account_info(
     {
-        "type": st.secrets.connections.gcs.type,
-        "project_id": st.secrets.connections.gcs.project_id,
-        "private_key_id": st.secrets.connections.gcs.private_key_id,
-        "private_key": st.secrets.connections.gcs.private_key,
-        "client_email": st.secrets.connections.gcs.client_email,
-        "client_id": st.secrets.connections.gcs.client_id,
-        "auth_uri": st.secrets.connections.gcs.auth_uri,
-        "token_uri": st.secrets.connections.gcs.token_uri,
-        "auth_provider_x509_cert_url": st.secrets.connections.gcs.auth_provider_x509_cert_url,
-        "client_x509_cert_url": st.secrets.connections.gcs.client_x509_cert_url
+        "type": st.secrets.type,
+        "project_id": st.secrets.project_id,
+        "private_key_id": st.secrets.private_key_id,
+        "private_key": st.secrets.private_key,
+        "client_email": st.secrets.client_email,
+        "client_id": st.secrets.client_id,
+        "auth_uri": st.secrets.auth_uri,
+        "token_uri": st.secrets.token_uri,
+        "auth_provider_x509_cert_url": st.secrets.auth_provider_x509_cert_url,
+        "client_x509_cert_url": st.secrets.client_x509_cert_url
     },
 )
 
